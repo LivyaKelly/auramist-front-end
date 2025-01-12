fbb6209b72d429ea2921615fcccbac55fbc1ccc6
Livya Kelly <livyakellysilva.lk@gmail.com>
2025-01-12 17:45:37 -0300

    Criando o projeto do zero

---
+++ auramist-front-end/src/pages/index.js
@@ -0,0 +1,44 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "@/styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <div className={styles.logo}>
+        <Image
+          src="/img/AURAMIST-Logo.svg"
+          alt="Auramist Logo"
+          width={400}
+          height={200}
+        />
+      </div>
+      <div className={styles.mulher}>
+        <Image
+          className={styles.mulherMeditando}
+          src="/img/Mulher_meditando.svg"
+          alt="Mulher meditando"
+          width={400}
+          height={200}
+        />
+        <Image
+          className={styles.elemento}
+          src="/img/Elemento-azul.svg"
+          alt="Elemento azul"
+          width={400}
+          height={200}
+        />
+      </div>
+      <div>
+        <h1 className={styles.boasVindas}>Bem-vindo ao Auramist!</h1>
+      </div>
+      <div>
+        <h3 className={styles.aviso}>Siga os passos para agendar seu próximo serviço de beleza</h3>
+      </div>
+      <div className={styles.botoes}>
+        <button className={styles.botaoP}>Entrar como Profissional</button>
+        <button className={styles.botaoC}>Entrar como<br/> Cliente</button>
+      </div>
+    </>
+  );
+}
